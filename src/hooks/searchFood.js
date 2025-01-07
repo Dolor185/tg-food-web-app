5c28642b15c8386cffac0955865e8e9b582c32b6
Dolor <kir.anishenko@gmail.com>
2025-01-06 18:35:39 -0700

    Update food search API endpoint to production URL

--- src/hooks/searchFood.js
+++ src/hooks/searchFood.js
@@ -2,9 +2,12 @@ import axios from "axios";
 
 export const searchFood = async (query, page = 0) => {
   try {
-    const response = await axios.get("http://localhost:3000/food-search", {
-      params: { query, page },
-    });
+    const response = await axios.get(
+      "http://tgfood-production.up.railway.app/food-search",
+      {
+        params: { query, page },
+      }
+    );
     // Проверяем, что в ответе есть объект 'foods' и массив 'food'
     if (response.data && response.data.foods && response.data.foods.food) {
       return response.data.foods.food; // Возвращаем массив продуктов
