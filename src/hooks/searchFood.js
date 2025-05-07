324a16dc0068057f422b7307d5bf0c735cc45125
Dolor185 <kir.anishenko@gmail.com>
2025-05-06 20:39:04 -0600

    Refactor searchFood function to simplify response handling and return food results directly

--- src/hooks/searchFood.js
+++ src/hooks/searchFood.js
@@ -1,34 +1,24 @@
 import axios from "axios";
 const url = process.env.REACT_APP_URL;
-
 export const searchFood = async (query, page = 0) => {
   try {
+    // http://localhost:3000/food-search
+    // https://tgfood-production.up.railway.app/food-search
+
     const response = await axios.get(`${url}/food-search`, {
       params: { query, page },
     });
 
-    const allFoods =
-      response.data?.foods_search?.results?.food || [];
-
-    const words = query
-      .toLowerCase()
-      .split(/\s+/)
-      .filter(Boolean); // убираем пустые строки
-
-    const filteredFoods = allFoods.filter((food) => {
-      const name = (food.food_name || "").toLowerCase();
-      const brand = (food.brand_name || "").toLowerCase();
-
-      const combined = `${name} ${brand}`;
-
-      return words.every((word) => combined.includes(word));
-    });
-
-    console.log("Original count:", allFoods.length);
-    console.log("Filtered count:", filteredFoods.length);
-    console.log("Words:", words);
-    console.log("API response:", allFoods);
-    return allFoods;
+    if (
+      response.data &&
+      response.data.foods_search &&
+      response.data.foods_search.results.food
+    ) {
+      console.log(response.data);
+      return response.data.foods_search.results.food;
+    } else {
+      return []; // Если нет продуктов, возвращаем пустой массив
+    }
   } catch (error) {
     console.error("Error searching for food:", error.message);
     return [];
